fcad772e54745bdc8dedec4dadac50f26d742599
Kamran Bhatti <kamranbhatti03072001@gmail.com>
2021-08-11 11:07:53 +0100

    Added rate limit

--- BotWebService/__main__.py
+++ BotWebService/__main__.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import aiohttp
 
 from aiohttp import web
@@ -18,8 +19,38 @@ async def issue_opened_event(event, gh, *args, **kwargs):
     url = event.data["issue"]["comments_url"]
     author = event.data["issue"]["user"]["login"]
 
-    message = f"Thanks @{author}!\n\nI will report this to Kamran ASAP!\n\n--Maid-Chan."
+    message = f"Thanks @{author}!\n\n\nI will report this to Kamran ASAP!\n\n\n-- Maid-Chan."
     await gh.post(url, data={"body": message})
+
+    await rate_limit_comment(event, gh)
+
+async def rate_limit_comment(event, gh):
+    comments_url = event.data["pull_request"]["comments_url"]
+    rate_limit = gh.rate_limit
+    remaining = rate_limit.remaining
+    total = rate_limit.limit
+    reset_datetime = rate_limit.reset_datetime
+
+    if remaining <= 10:
+	message = f"\**:WARNING::WARNING::WARNING::WARNING::WARNING:WARNING**:\n\n\nMaid-Chan is reaching near my API limit.\nI have only {remaining} of {total} API requests left. They will reset on {reset_datetime} (GMT), which is in {reset_datetime - datetime.datetime.now(datetime.timezone.gmt)}."
+    await gh.post(comments_url, data={"body": message})
+
+
+@router.register("pull_request", action="closed")
+async def pull_request_closed_event(event, gh, *args, **kwargs):
+    """
+    Whenever a pull request is closed, check if it has been merged or simply closed
+    """
+    url = event.data["pull_request"]["issue_url"]
+    pr_number = event.data["pull_request"]["number"]
+    message = f"Pull Request #{pr_number} was {event.data['action']}."
+    if not event.data['pull_request']['merged']:
+        message += "\n\n\nIt was closed without merging, skipping\n\n\n-- Maid-Chan"
+    else:
+	message += "\n\n\nIt was merged to the code base. Heres a cookie: 🍪\n\n\n-- Maid-Chan"
+    await gh.post(url, data={"body": message})
+
+    await rate_limit_comment(event, gh)
 
 @routes.post("/")
 async def main(request):
